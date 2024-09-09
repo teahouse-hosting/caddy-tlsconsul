1be21d2cef4e993190f32a97b93fafbb724d758d
Peter Teich <peter.teich@gmail.com>
2024-09-09 19:30:02 +0200

    Refactor tests to use testcontainers

--- storage_test.go
+++ storage_test.go
@@ -1,39 +1,59 @@
-// +build consul
-
 package storageconsul
 
 import (
-	"github.com/caddyserver/certmagic"
-	consul "github.com/hashicorp/consul/api"
-	"github.com/stretchr/testify/assert"
+	"context"
+	"io/fs"
 	"os"
 	"path"
 	"testing"
 	"time"
+
+	"github.com/caddyserver/caddy/v2"
+	consul "github.com/hashicorp/consul/api"
+	"github.com/stretchr/testify/assert"
+
+	consulContainer "github.com/testcontainers/testcontainers-go/modules/consul"
 )
 
-var consulClient *consul.Client
+var consulCont *consulContainer.ConsulContainer
 
 const TestPrefix = "consultlstest"
 
-// these tests needs a running Consul server
+func TestMain(m *testing.M) {
+	cc, err := consulContainer.Run(context.Background(), "hashicorp/consul:1.15")
+	if err != nil {
+		panic(err)
+	}
+
+	consulCont = cc
+	defer consulCont.Terminate(context.Background())
+
+	m.Run()
+}
+
+// these tests need a running Consul server
 func setupConsulEnv(t *testing.T) *ConsulStorage {
-
 	os.Setenv(EnvNamePrefix, TestPrefix)
-	os.Setenv(consul.HTTPTokenEnvName, "2f9e03f8-714b-5e4d-65ea-c983d6b172c4")
-
-	cs, err := New()
+	consulEndpoint, err := consulCont.ApiEndpoint(context.Background())
 	assert.NoError(t, err)
+
+	//os.Setenv(consul.HTTPTokenEnvName, "2f9e03f8-714b-5e4d-65ea-c983d6b172c4")
+	os.Setenv(consul.HTTPAddrEnvName, consulEndpoint)
+
+	cs := New()
+	ctx, _ := caddy.NewContext(caddy.Context{Context: context.Background()})
+	cs.Provision(ctx)
 
 	_, err = cs.ConsulClient.KV().DeleteTree(TestPrefix, nil)
 	assert.NoError(t, err)
+
 	return cs
 }
 
 func TestConsulStorage_Store(t *testing.T) {
 	cs := setupConsulEnv(t)
 
-	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt data"))
+	err := cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt data"))
 	assert.NoError(t, err)
 }
 
@@ -42,10 +62,10 @@ func TestConsulStorage_Exists(t *testing.T) {
 
 	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
 
-	err := cs.Store(key, []byte("crt data"))
+	err := cs.Store(context.Background(), key, []byte("crt data"))
 	assert.NoError(t, err)
 
-	exists := cs.Exists(key)
+	exists := cs.Exists(context.Background(), key)
 	assert.True(t, exists)
 }
 
@@ -55,10 +75,10 @@ func TestConsulStorage_Load(t *testing.T) {
 	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
 	content := []byte("crt data")
 
-	err := cs.Store(key, content)
+	err := cs.Store(context.Background(), key, content)
 	assert.NoError(t, err)
 
-	contentLoded, err := cs.Load(key)
+	contentLoded, err := cs.Load(context.Background(), key)
 	assert.NoError(t, err)
 
 	assert.Equal(t, content, contentLoded)
@@ -70,20 +90,19 @@ func TestConsulStorage_Delete(t *testing.T) {
 	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
 	content := []byte("crt data")
 
-	err := cs.Store(key, content)
+	err := cs.Store(context.Background(), key, content)
 	assert.NoError(t, err)
 
-	err = cs.Delete(key)
+	err = cs.Delete(context.Background(), key)
 	assert.NoError(t, err)
 
-	exists := cs.Exists(key)
+	exists := cs.Exists(context.Background(), key)
 	assert.False(t, exists)
 
-	contentLoaded, err := cs.Load(key)
+	contentLoaded, err := cs.Load(context.Background(), key)
 	assert.Nil(t, contentLoaded)
-
-	_, ok := err.(certmagic.ErrNotExist)
-	assert.True(t, ok)
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, fs.ErrNotExist)
 }
 
 func TestConsulStorage_Stat(t *testing.T) {
@@ -92,10 +111,10 @@ func TestConsulStorage_Stat(t *testing.T) {
 	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
 	content := []byte("crt data")
 
-	err := cs.Store(key, content)
+	err := cs.Store(context.Background(), key, content)
 	assert.NoError(t, err)
 
-	info, err := cs.Stat(key)
+	info, err := cs.Stat(context.Background(), key)
 	assert.NoError(t, err)
 
 	assert.Equal(t, key, info.Key)
@@ -104,14 +123,14 @@ func TestConsulStorage_Stat(t *testing.T) {
 func TestConsulStorage_List(t *testing.T) {
 	cs := setupConsulEnv(t)
 
-	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
+	err := cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
 	assert.NoError(t, err)
-	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
+	err = cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
 	assert.NoError(t, err)
-	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
+	err = cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
 	assert.NoError(t, err)
 
-	keys, err := cs.List(path.Join("acme", "example.com", "sites", "example.com"), true)
+	keys, err := cs.List(context.Background(), path.Join("acme", "example.com", "sites", "example.com"), true)
 	assert.NoError(t, err)
 	assert.Len(t, keys, 3)
 	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"))
@@ -120,14 +139,14 @@ func TestConsulStorage_List(t *testing.T) {
 func TestConsulStorage_ListNonRecursive(t *testing.T) {
 	cs := setupConsulEnv(t)
 
-	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
+	err := cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
 	assert.NoError(t, err)
-	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
+	err = cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
 	assert.NoError(t, err)
-	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
+	err = cs.Store(context.Background(), path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
 	assert.NoError(t, err)
 
-	keys, err := cs.List(path.Join("acme", "example.com", "sites"), false)
+	keys, err := cs.List(context.Background(), path.Join("acme", "example.com", "sites"), false)
 	assert.NoError(t, err)
 
 	assert.Len(t, keys, 1)
@@ -138,10 +157,10 @@ func TestConsulStorage_LockUnlock(t *testing.T) {
 	cs := setupConsulEnv(t)
 	lockKey := path.Join("acme", "example.com", "sites", "example.com", "lock")
 
-	err := cs.Lock(lockKey)
+	err := cs.Lock(context.Background(), lockKey)
 	assert.NoError(t, err)
 
-	err = cs.Unlock(lockKey)
+	err = cs.Unlock(context.Background(), lockKey)
 	assert.NoError(t, err)
 }
 
@@ -150,17 +169,17 @@ func TestConsulStorage_TwoLocks(t *testing.T) {
 	cs2 := setupConsulEnv(t)
 	lockKey := path.Join("acme", "example.com", "sites", "example.com", "lock")
 
-	err := cs.Lock(lockKey)
+	err := cs.Lock(context.Background(), lockKey)
 	assert.NoError(t, err)
 
 	go time.AfterFunc(5*time.Second, func() {
-		err = cs.Unlock(lockKey)
+		err = cs.Unlock(context.Background(), lockKey)
 		assert.NoError(t, err)
 	})
 
-	err = cs2.Lock(lockKey)
+	err = cs2.Lock(context.Background(), lockKey)
 	assert.NoError(t, err)
 
-	err = cs2.Unlock(lockKey)
+	err = cs2.Unlock(context.Background(), lockKey)
 	assert.NoError(t, err)
 }
